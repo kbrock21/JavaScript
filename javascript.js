0b03e0a7f8a7aefaf65cddc87f5b2b5be9b4fcbe
kbrock21 <101204770+kbrock21@users.noreply.github.com>
2024-01-15 17:14:00 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function add7(num) {
+    return(num +7);
+}
+
+function multiply(x,y){
+    return (x*y);
+    }
+let result = multiply(x,y);
+console.log(result);
+
+function capitalize(string) {
+    return string.charAt(0).toUpperCase()+string.slice(1).toLowerCase();
+
+}
+console.log(capitalize('wElCoMe'));
